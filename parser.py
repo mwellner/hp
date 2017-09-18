e61960861f0d1a2bd4e0453179b1a318f1d2e047
Mathias Wellner <mathias@mwellner.de>
2017-09-18 22:27:38 +0200

    updated parser, which now sets lastmod for all files

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import os.path
+import datetime
 
 folder = 'content/post'
 extension = '.md'
@@ -7,21 +8,23 @@ def step(ext, dirname, names):
     ext = ext.lower()
     for name in names:
         if name.lower().endswith(ext):
+            modified_ts = os.path.getmtime(os.path.join(dirname, name))
+            modified = datetime.datetime.fromtimestamp(modified_ts).strftime('%Y-%m-%dT%H:%M:%S')
             with open(os.path.join(dirname, name), 'r') as fh:
                 lines = fh.readlines()
-                modified = [l for l in lines if isvalidline(l)]
-            with open(os.path.join(dirname, name), 'w') as fh:
-                fh.writelines(modified)
 
-def isvalidline(line):
-    if line.startswith('layout: '):
-        return False
-    if line.startswith('id: '):
-        return False
-    if line.startswith('guid: '):
-        return False
-    if line.startswith('permalink: '):
-        return False
-    return True
+            was_changed = False
+            for i, line in enumerate(lines):
+                if line.startswith('lastmod: '):
+                    lines[i] = 'lastmod: ' + modified + '+00:00\n'
+                    was_changed = True
+            if not was_changed:
+                for i, line in enumerate(lines):
+                    if line.startswith('date: '):
+                        date_index = i
+                lines.insert(date_index + 1, 'lastmod: ' + modified + '+00:00\n')
+
+            with open(os.path.join(dirname, name), 'w') as fh:
+                fh.writelines(lines)
 
 os.path.walk(folder, step, extension)

bd30a822149a756638e9de6dcf3d9372dc866762
Mathias Wellner <mathias@mwellner.de>
2018-11-19 23:59:20 +0100

    modify build process for js and css

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require("gulp");
+const del = require("del");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass");
+
+const jsFiles = ["node_modules/jquery/dist/jquery.js", "node_modules/tether/dist/js/tether.js", "node_modules/bootstrap/dist/js/bootstrap.bundle.js", "source/js/*.js"];
+const jsDest = "static/js";
+gulp.task("scripts", () => gulp.src(jsFiles)
+    .pipe(concat("bundle.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest))
+);
+
+const cssFiles = "source/scss/style.scss";
+const cssDest = "static/css";
+gulp.task("css", () => gulp.src(cssFiles)
+    .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
+    .pipe(gulp.dest(cssDest))
+);

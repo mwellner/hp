963c75f7a2d55ada92a086839252f84a534d5d9a
Mathias Wellner <mathias@mwellner.de>
2018-11-20 00:14:53 +0100

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const rename = require("gulp-rename");
 const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
 const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
 
 const jsFiles = ["node_modules/jquery/dist/jquery.js", "node_modules/tether/dist/js/tether.js", "node_modules/bootstrap/dist/js/bootstrap.bundle.js", "source/js/*.js"];
 const jsDest = "static/js";
@@ -17,5 +18,6 @@ const cssFiles = "source/scss/style.scss";
 const cssDest = "static/css";
 gulp.task("css", () => gulp.src(cssFiles)
     .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
+    .pipe(postcss())
     .pipe(gulp.dest(cssDest))
 );

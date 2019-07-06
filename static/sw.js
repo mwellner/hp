6b3e9150052acb4e09dedf9ba35114bbb72c91c1
Mathias Wellner <mathias@mwellner.de>
2019-07-06 13:55:33 +0200

    remove beforeinstallprompt callback

--- static/sw.js
+++ static/sw.js
@@ -63,30 +63,6 @@
     return url.indexOf('.jpg') !== -1 || url.indexOf('.png') !== -1;
   }
 
-  self.addEventListener('beforeinstallprompt', function(event) {
-    console.log("beforeinstall");
-    event.preventDefault();
-    deferredPrompt = event;
-    var installButton = document.querySelector('#install-app-button');
-    installButton.style.display = 'block';
-    installButton.addEventListener('click', (e) => {
-      // hide our user interface that shows our A2HS button
-      installButton.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice
-        .then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          deferredPrompt = null;
-        });
-    });
-  });
-
   self.addEventListener('install', function (event) {
     event.waitUntil(updateStaticCache()
       .then(function () {

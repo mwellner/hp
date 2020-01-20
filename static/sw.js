7c998bdf146c7440f4f5a57e7ba4052c93ecfa77
Mathias Wellner <mathias@mwellner.de>
2020-01-20 23:53:31 +0100

    update service worker

--- static/sw.js
+++ static/sw.js
@@ -1,109 +1,107 @@
-'use strict';
+"use strict";
 
-(function () {
-  var isLocalhost = Boolean(self.location.hostname === 'localhost');
+(function() {
+  var isLocalhost = Boolean(self.location.hostname === "localhost");
   if (isLocalhost) {
-    console.log("Detected localhost, service worker does not cache requests");    
+    console.log("Detected localhost, service worker does not cache requests");
     return;
   }
 
-  var staticCacheName = 'static';
-  var pagesCacheName = 'pages';
-  var imagesCacheName = 'images';
-  var version = 'v8::';
+  var staticCacheName = "static";
+  var pagesCacheName = "pages";
+  var imagesCacheName = "images";
+  var version = "v8::";
   var staticAssets = [
-    '/css/style8.css',
-    '/images/wmo_stripes_dark.png',
-    '/images/wmo_stripes_bg.png'
+    "/css/style8.css",
+    "/images/wmo_stripes_dark.png",
+    "/images/wmo_stripes_bg.png",
+    "/js/jquery-3.4.1.slim.min.js",
+    "/js/popper.min.js",
+    "/js/bootstrap.min.js",
+    "/js/fontawesome.min.js",
+    "/js/solid.min.js"
   ];
 
-  var updateStaticCache = function () {
-    return caches.open(version + staticCacheName)
-      .then(function (cache) {
-        return cache.addAll(staticAssets);
-      });
-  }
+  var updateStaticCache = function() {
+    return caches.open(version + staticCacheName).then(function(cache) {
+      return cache.addAll(staticAssets);
+    });
+  };
 
-  var stashInCache = function (cacheName, request, response) {
-    caches.open(cacheName)
-      .then(function (cache) {
-        cache.put(request, response);
-      });
-  }
+  var stashInCache = function(cacheName, request, response) {
+    caches.open(cacheName).then(function(cache) {
+      cache.put(request, response);
+    });
+  };
 
-  var trimCache = function (cacheName, maxItems) {
-    caches.open(cacheName)
-      .then(function (cache) {
-        cache.keys()
-          .then(function (keys) {
-            if (keys.length > maxItems) {
-              cache.delete(keys[0])
-                .then(trimCache(cacheName, maxItems));
-            }
-          });
+  var trimCache = function(cacheName, maxItems) {
+    caches.open(cacheName).then(function(cache) {
+      cache.keys().then(function(keys) {
+        if (keys.length > maxItems) {
+          cache.delete(keys[0]).then(trimCache(cacheName, maxItems));
+        }
       });
-  }
+    });
+  };
 
-  var clearOldCaches = function () {
-    return caches.keys()
-      .then(function (keys) {
-        return Promise.all(
-          keys
-            .filter(function (key) {
-              return key.indexOf(version) !== 0;
-            })
-            .map(function (key) {
-              return caches.delete(key);
-            })
-        );
-      });
-  }
+  var clearOldCaches = function() {
+    return caches.keys().then(function(keys) {
+      return Promise.all(
+        keys
+          .filter(function(key) {
+            return key.indexOf(version) !== 0;
+          })
+          .map(function(key) {
+            return caches.delete(key);
+          })
+      );
+    });
+  };
 
   var isImageUrl = function(url) {
-    return url.indexOf('.jpg') !== -1 || url.indexOf('.png') !== -1;
-  }
+    return url.indexOf(".jpg") !== -1 || url.indexOf(".png") !== -1;
+  };
 
-  self.addEventListener('install', function (event) {
-    event.waitUntil(updateStaticCache()
-      .then(function () {
+  self.addEventListener("install", function(event) {
+    event.waitUntil(
+      updateStaticCache().then(function() {
         return self.skipWaiting();
       })
     );
   });
 
-  self.addEventListener('activate', function (event) {
-    event.waitUntil(clearOldCaches()
-      .then(function () {
+  self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      clearOldCaches().then(function() {
         return self.clients.claim();
       })
     );
   });
-  self.addEventListener('message', function (event) {
-    if (event.data.command == 'trimCaches') {
+  self.addEventListener("message", function(event) {
+    if (event.data.command == "trimCaches") {
       trimCache(version + pagesCacheName, 35);
       trimCache(version + imagesCacheName, 20);
     }
   });
 
-  self.addEventListener('fetch', function (event) {
+  self.addEventListener("fetch", function(event) {
     var request = event.request;
     // For non-GET requests, try the network first, fall back to the index page
-    if (request.method !== 'GET') {
+    if (request.method !== "GET") {
       event.respondWith(
-        fetch(request)
-          .catch(function () {
-            return caches.match('/');
-          })
+        fetch(request).catch(function() {
+          return caches.match("/");
+        })
       );
       return;
     }
 
     // For HTML requests, try the network first, fall back to the cache, finally the offline page
-    if (request.headers.get('Accept').indexOf('text/html') !== -1) {
+    if (request.headers.get("Accept").indexOf("text/html") !== -1) {
       // Fix for Chrome bug: https://code.google.com/p/chromium/issues/detail?id=573937
-      if (request.mode != 'navigate') {
+      if (request.mode != "navigate") {
         request = new Request(request.url, {
-          method: 'GET',
+          method: "GET",
           headers: request.headers,
           mode: request.mode,
           credentials: request.credentials,
@@ -112,7 +110,7 @@
       }
       event.respondWith(
         fetch(request)
-          .then(function (response) {
+          .then(function(response) {
             // NETWORK
             // Stash a copy of this page in the pages cache
             var copy = response.clone();
@@ -120,12 +118,11 @@
             stashInCache(cacheName, request, copy);
             return response;
           })
-          .catch(function () {
+          .catch(function() {
             // CACHE or FALLBACK
-            return caches.match(request)
-              .then(function (response) {
-                return response || caches.match('/');
-              })
+            return caches.match(request).then(function(response) {
+              return response || caches.match("/");
+            });
           })
       );
       return;
@@ -133,11 +130,12 @@
 
     // For non-HTML requests, look in the cache first, fall back to the network
     event.respondWith(
-      caches.match(request)
-        .then(function (response) {
-          // CACHE
-          return response || fetch(request)
-            .then(function (response) {
+      caches.match(request).then(function(response) {
+        // CACHE
+        return (
+          response ||
+          fetch(request)
+            .then(function(response) {
               // NETWORK
               // If the request is for an image, stash a copy of this image in the images cache
               if (isImageUrl(request.url)) {
@@ -147,14 +145,18 @@
               }
               return response;
             })
-            .catch(function () {
+            .catch(function() {
               // OFFLINE
               // If the request is for an image, show an offline placeholder
               if (isImageUrl(request.url)) {
-                return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' } });
+                return new Response(
+                  '<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>',
+                  { headers: { "Content-Type": "image/svg+xml" } }
+                );
               }
-            });
-        })
-    );    
+            })
+        );
+      })
+    );
   });
 })();
